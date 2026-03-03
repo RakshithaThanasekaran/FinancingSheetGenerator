59e5179f24602dfe563edd2dd35ba195fb28bcd9
Rakshitha Thanasekaran <trakshitha10@gmail.com>
2026-03-02 20:45:34 -0500

    set up a basic website that we can then modify once we have the logic set up

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+    """
+    Homepage view: renders the skeleton of the finance sheet generator
+    """
+    return render(request, "main/home.html")
